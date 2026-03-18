0794105cfe8745458905093c0e50cabaa8281d69
S.Yunseo <sonyunsea@gmail.com>
2026-03-18 17:52:04 +0900

    0317 코딩테스트

---
+++ coding-test/ct02/q4.js
@@ -0,0 +1,10 @@
+//두 수의 합 구하기
+//정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+function solution(num1, num2) {
+    var answer = -1
+    var answer = num1 + num2;
+    return answer;
+}
+
+// -50,000 ≤ num1 ≤ 50,000
+// -50,000 ≤ num2 ≤ 50,000
